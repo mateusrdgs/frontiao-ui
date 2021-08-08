c6acb7674e66a85704337a9d593203fa2e32fdca
Mateus Rodrigues <devrdgs@gmail.com>
2021-08-08 18:07:59 -0300

    fix: logging hello world on button init

--- packages/button/lib/index.js
+++ packages/button/lib/index.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import classnames from 'classnames'
 
 const Button = ({ className, ...props }) => {
     const classNames = classnames('button', {
         [className]: className
     })
+
+    useEffect(() => {
+        console.log('hello world')
+    }, [])
 
     return (
         <button {...props} className={classNames} />
