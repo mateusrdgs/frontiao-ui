11047fdb03c0e4966ea4fc087171edf5b77bc86c
Mateus Rodrigues <devrdgs@gmail.com>
2021-08-09 14:17:59 -0300

    refactor: removing useEffect from button component

--- packages/button/lib/index.js
+++ packages/button/lib/index.js
@@ -1,14 +1,10 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import classnames from 'classnames'
 
 const Button = ({ className, ...props }) => {
     const classNames = classnames('button', {
         [className]: className
     })
-
-    useEffect(() => {
-        console.log('hello world')
-    }, [])
 
     return (
         <button {...props} className={classNames} />
