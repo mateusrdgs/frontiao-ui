2df673f38c4e760c4c0efbbed72ed9418ad41bcf
Mateus Rodrigues <devrdgs@gmail.com>
2021-08-09 14:56:12 -0300

    refactor: removed useless console.log

--- packages/button/lib/index.js
+++ packages/button/lib/index.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import classnames from 'classnames'
 
-console.log('hello world!')
-
 const Button = ({ className, ...props }) => {
     const classNames = classnames('button', {
         [className]: className
