759cbd92cc6583c6ac14787654cb2796fb21c95e
Mateus Rodrigues <devrdgs@gmail.com>
2021-08-09 14:46:18 -0300

    fix: testing custom version message

--- packages/button/lib/index.js
+++ packages/button/lib/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import classnames from 'classnames'
 
+console.log('hello world')
+
 const Button = ({ className, ...props }) => {
     const classNames = classnames('button', {
         [className]: className
