640c54d4a08af890aae1bfef910add42c9c8f5e8
Mateus Rodrigues <devrdgs@gmail.com>
2021-08-09 18:41:59 -0300

    refactor: updating log message

--- packages/badge/lib/index.js
+++ packages/badge/lib/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import classnames from 'classnames'
 
-console.log('testing build again')
+console.log('testing build again 2')
 
 const Badge = ({ className, ...props }) => {
     const classNames = classnames('badge', {
