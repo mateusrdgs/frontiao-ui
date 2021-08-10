ace1a7cf51d87f70a881bdb7dab1ee012a8c607a
Mateus Rodrigues <devrdgs@gmail.com>
2021-08-10 17:45:18 -0300

    fix: styling task label

--- packages/task/lib/index.js
+++ packages/task/lib/index.js
@@ -5,7 +5,7 @@ const Task = ({ id, description, done, onCheck }) => {
     <li className={style.task}>
       <input className={style.input} type="checkbox" id={id} defaultChecked={done} onChange={onCheck(id)} />
       <span className={style.mark} onClick={onCheck(id)} />
-      <label htmlFor={id}>
+      <label className={style.label} htmlFor={id}>
         { description }
       </label>
     </li>
