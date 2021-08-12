d45e9abef79f1f6b257863fb03aef361c77112e3
Mateus Rodrigues <devrdgs@gmail.com>
2021-08-12 19:57:13 -0300

    feat: added button to header

--- packages/header/lib/index.js
+++ packages/header/lib/index.js
@@ -1,14 +1,18 @@
 import classnames from 'classnames'
+import Button from '@frontiao/button'
 
 import style from './header.scss';
 
-const Header = ({ className, ...props }) => {
+const Header = ({ className, children, ...props }) => {
   const classNames = classnames(style.header, {
     [className]: className
   })
 
   return (
-    <header {...props} className={classNames} />
+    <header className={classNames} {...props}>
+      { children }
+      <Button>novo botao</Button>
+    </header>
   )
 }
 
